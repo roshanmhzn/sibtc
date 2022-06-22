4eac89c75231d24a1f088c2a28c46cd8a3cad5db
Maharjan <roshan.maharjan@cotiviti.com>
2022-06-22 17:12:52 +0545

    Fundamentals

--- boards/tests.py
+++ boards/tests.py
@@ -152,6 +152,18 @@ class NewTopicTests(TestCase):
         response = self.client.get(url)
         form = response.context.get('form')
         self.assertIsInstance(form, NewTopicForm)
+    
+    def test_new_topic_invalid_post_data(self):  # <- updated this one
+        '''
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        form = response.context.get('form')
+        self.assertEquals(response.status_code, 200)
+        # print(form.errors)
+        self.assertTrue(form.errors)
 
     
 
